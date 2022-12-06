6272c64ba76ee71578d25d11b51a93b95dd5a49c
Mumtahin Farabi <mfarabi619@gmail.com>
2022-12-05 21:09:21 -0500

    Commented out boiletplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,35 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.scss'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      </div> */}
+
+
+      {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+        Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-      </div>
-      <p className="read-the-docs">
+      </div> */}
+      {/* <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+
+<p>Created with Vite + React by Mumtahin Farabi</p>
     </div>
   )
 }
